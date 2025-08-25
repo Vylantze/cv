c1a342984ae9ca6872d10fa9e8ddc0a353d48367
Vylantze <digi9346@hotmail.com>
2025-08-25 10:07:51 +0800

    Add configs. Update to 0.1.2

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
@@ -19,5 +23,23 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+
+      // stylistic
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single', { 'avoidEscape': true }],
+
+      // spacing related
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/no-whitespace-before-property': 'error',
+      '@stylistic/rest-spread-spacing': ['error', 'never'],
+      '@stylistic/space-before-blocks': 'error',
+      '@stylistic/space-before-function-paren': 'error',
+      '@stylistic/spaced-comment': ['error', 'always'],
+      '@stylistic/space-infix-ops': 'error',
+      '@stylistic/switch-colon-spacing': ['error', { 'after': true, 'before': false }],
+    }
   },
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import {
+  configDefaults,
+  coverageConfigDefaults
+} from 'vitest/config';
+
+const testExcludes = [
+  '/dist/**',
+  'src/main.tsx',
+  'public/**',
+];
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cv/',
-})
+  base: '/resume/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@test': path.resolve(__dirname, './tests'),
+    },
+  },
+  test: {
+    ...configDefaults,
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/vitest.setup.ts',
+    silent: true,
+    css: true,
+    mockReset: true,
+    restoreMocks: true,
+    clearMocks: true,
+    exclude: [
+      ...configDefaults.exclude,
+      ...testExcludes
+    ],
+    coverage: {
+      ...coverageConfigDefaults,
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        ...testExcludes,
+      ]
+    }
+  }
+});
+
